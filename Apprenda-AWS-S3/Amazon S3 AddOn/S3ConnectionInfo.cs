e217c006189783beccfcad4cb27329910ca4bdae
Chris Dutra <cdutra@apprenda.com>
2016-10-24 09:20:24 -0400

    changed connection string implementation, json does not escape properly in app.config of .NET apps, and overall is unnecessary.

--- Apprenda-AWS-S3/Amazon S3 AddOn/S3ConnectionInfo.cs
+++ Apprenda-AWS-S3/Amazon S3 AddOn/S3ConnectionInfo.cs
@@ -11,18 +11,18 @@ namespace Apprenda.SaaSGrid.Addons.AWS.S3
         {
             try
             {
-                var info = JsonConvert.DeserializeObject<S3ConnectionInfo>(_connectionInfo);
-                return info;
+                // var info = JsonConvert.DeserializeObject<S3ConnectionInfo>(_connectionInfo);
+                return new S3ConnectionInfo() { BucketName = _connectionInfo };
             }
             catch (Exception)
             {
-                throw new ArgumentException("Deserialization failed, check your provisioned addon instance data.");
+                throw new ArgumentException("Parse failed, check your provisioned addon instance data.");
             }
         }
 
         public override string ToString()
         {
-            return JsonConvert.SerializeObject(this);
+            return this.BucketName;
         }
     }
 }
