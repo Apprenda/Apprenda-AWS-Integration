39b59b2208c76960082e0985edc244dd5d48f715
Michael Michael <michmike@cs.stanford.edu>
2017-12-22 14:24:51 -0600

    Updating build script

--- CreateArchives.ps1
+++ CreateArchives.ps1
@@ -1,5 +1,6 @@
 $directoryPrefix = "C:\Users\mmichael\Documents\GitHub\Apprenda-AWS-Integration\Apprenda-AWS-Build\"
-$directories = @("Glacier","Lambda", "RDS","Redshift", "S3", "SNS", "SQS", "AMI")
+#$directories = @("Glacier","Lambda", "RDS","Redshift", "S3", "SNS", "SQS", "AMI")
+$directories = @("RDS","S3")
 
 Add-Type -Assembly System.IO.Compression.FileSystem
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
@@ -7,7 +8,7 @@ $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
 foreach ($directoryFolder in $directories)
 {
     $fullPath = $directoryPrefix + $directoryFolder    
-    $zipFileResult = "$($directoryPrefix)\$($directoryFolder).zip"
+    $zipFileResult = "$($directoryPrefix)\Apprenda.AddOn.Amazon.AWS.$($directoryFolder).zip"
 
     if (Test-Path $zipFileResult) 
     {
